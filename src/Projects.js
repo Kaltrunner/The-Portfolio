3372efa4843f0f4e4f45ee3e754d2d0dbd2a9022
Erik Russell <erikrussells@gmail.com>
2023-03-23 22:34:30 -0400

    updated green square link

--- src/Projects.js
+++ src/Projects.js
@@ -1,31 +1,8 @@
 import Footer from "./Footer";
 import { Link } from "react-router-dom";
 import { ScrollToTop } from "react-router-scroll-to-top";
-// import {
-//   Animator,
-//   ScrollContainer,
-//   ScrollPage,
-//   batch,
-//   Fade,
-//   FadeIn,
-//   FadeOut,
-//   Move,
-//   MoveIn,
-//   MoveOut,
-//   Sticky,
-//   StickyIn,
-//   StickyOut,
-//   Zoom,
-//   ZoomIn,
-//   ZoomOut,
-// } from "react-scroll-motion";
 
 function Projects() {
-  // const location = useLocation();
-  // console.log(location);
-
-//   const moveinleft = batch(Sticky(35, 60), FadeOut(0, -900));
-
   return (
     <>
       <div className="projects-body-div">
@@ -40,26 +17,21 @@ function Projects() {
 
         <div className="projects-header-text-div">
           <h1 className="header-text">Projects</h1>
-          <h4 className="projects-sub-header" >2022 - present</h4>
+          <h4 className="projects-sub-header">2022 - present</h4>
         </div>
 
         <div className="line-div"></div>
 
         <div className="projects-container">
-        
           <div className="horizontalScroll">
-            
             <div class="external">
-      
               <div className="horizontal-scroll-wrapper">
-
-            {/* <div class="img-wrapper faster" id="invisible-img">
-                  <img className="projects-image" src="rudy.png" alt="" />
-                </div> */}
-
-
                 <div className="img-wrapper">
-                <a href="https://www.erikmrussell.com/" target="_blank" rel="noreferrer">
+                  <a
+                    href="https://www.erikmrussell.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <img
                       className="projects-image"
                       src="Snake pit.png"
@@ -69,13 +41,9 @@ function Projects() {
                   </a>
                 </div>
 
-                {/* <div class="img-wrapper faster" id="invisible-img">
-                  <img className="projects-image" src="rudy.png" alt="" />
-                </div> */}
-
                 <div class="img-wrapper">
-                <a
-                    href="https://kaltrunner.github.io/agreensquare.github.io/"
+                  <a
+                    href="https://a-green-square-project.onrender.com/"
                     target="_blank"
                     rel="noreferrer"
                   >
@@ -90,12 +58,12 @@ function Projects() {
                   </a>
                 </div>
 
-                {/* <div class="img-wrapper slower slower-down" id="invisible-img">
-                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74321/swanduckriver.jpg" alt=""/>
-                </div> */}
-
                 <div class="img-wrapper">
-                <a href="https://optical-lie.onrender.com/" target="_blank" rel="noreferrer">
+                  <a
+                    href="https://optical-lie.onrender.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <img
                       className="projects-image"
                       src="optical-lie.png"
@@ -105,15 +73,12 @@ function Projects() {
                   </a>
                 </div>
 
-                {/* <div class="img-wrapper slower" id="invisible-img">
-                    <img
-                      src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74321/paris-seine-boat.jpg"
-                      alt=""
-                    />
-                </div> */}
-
                 <div class="img-wrapper">
-                <a href="https://www.erikmrussell.com/" target="_blank" rel="noreferrer">
+                  <a
+                    href="https://www.erikmrussell.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <img
                       className="projects-image"
                       src="rudy.png"
@@ -123,15 +88,12 @@ function Projects() {
                   </a>
                 </div>
 
-                {/* <div class="img-wrapper slower slower2" id="invisible-img">
-                    <img
-                      src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74321/cafe-table-street.jpg"
-                      alt=""
-                    />
-                </div> */}
-
                 <div class="img-wrapper">
-                <a href="https://nutimasafn.onrender.com/" target="_blank" rel="noreferrer">
+                  <a
+                    href="https://nutimasafn.onrender.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <img
                       className="projects-image"
                       src="Screenshot 2023-03-13 at 2.30.32 PM.png"
@@ -141,15 +103,12 @@ function Projects() {
                   </a>
                 </div>
 
-                {/* <div class="img-wrapper slower" id="invisible-img">
-                    <img
-                      src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74321/notre-dame-river-boat.jpg"
-                      alt=""
-                    />
-                </div> */}
-
                 <div class="img-wrapper">
-                <a href="https://design-system-sid.onrender.com/" target="_blank" rel="noreferrer">
+                  <a
+                    href="https://design-system-sid.onrender.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <img
                       className="projects-image"
                       src="design-system-sid.png"
@@ -158,22 +117,10 @@ function Projects() {
                     <p className="projects-image-text">Design System Sid.</p>
                   </a>
                 </div>
-
-                {/* <div class="img-wrapper slower" id="invisible-img">
-                    <img
-                      src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74321/notre-dame-river-boat.jpg"
-                      alt=""
-                    />
-                </div> */}
-
               </div>
-             
             </div>
-            
           </div>
-          
         </div>
-        {/* <div className="line-div"></div> */}
       </div>
       <Footer />
       <ScrollToTop />
