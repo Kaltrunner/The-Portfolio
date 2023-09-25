b45565dfee30594c21a127ef3e55e9790e3d8f60
Erik Russell <erikrussells@gmail.com>
2023-09-25 13:48:03 -0400

    updating footer for better mobile viewing

--- src/Footer.js
+++ src/Footer.js
@@ -3,49 +3,51 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <div className="footer-body-div">
+      {/* <div className="coordinates-div">
+        <p className="coordinates">
+        • 40.7305° N, 73.9515° W
+        </p>
+      </div> */}
       <div className="footer-link-list">
-        <ul>
-          <Link className="projects-link" to="/Projects">
-            <p className="footer-text-tag">All Projects</p>
-          </Link>
+        <Link className="projects-link" to="/Projects">
+          <p className="footer-text-tag">All Projects</p>
+        </Link>
 
-          <p className="footer-text">
-            <a
-              className="tag"
-              target="_blank"
-              rel="noreferrer"
-              href="https://github.com/Kaltrunner"
-            >
-              GitHub
-            </a>
-          </p>
+        <p className="footer-text">
+          <a
+            className="tag"
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Kaltrunner"
+          >
+            GitHub
+          </a>
+        </p>
 
-          <p className="footer-text">
-            <a
-              className="tag"
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.linkedin.com/in/erik-russell-software-engineer/"
-            >
-              LinkedIn
-            </a>
-          </p>
+        <p className="footer-text">
+          <a
+            className="tag"
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/erik-russell-software-engineer/"
+          >
+            LinkedIn
+          </a>
+        </p>
 
-          <p className="footer-logo">© 2023 Erik Russell</p>
-        </ul>
+        
       </div>
 
       <div id="email-tag">
-        <ul>
-          <p className="footer-text">
-            <a
-              className="tag"
-              href="mailto:erikrussells@gmail.com?subject=Hello!"
-            >
-              erikrussells@gmail.com
-            </a>
-          </p>
-        </ul>
+        <p className="footer-text">
+          <a
+            className="tag"
+            href="mailto:erikrussells@gmail.com?subject=Hello!"
+          >
+            erikrussells@gmail.com
+          </a>
+        </p>
+        <p className="footer-copy-right">© 2023 Erik Russell</p>
       </div>
     </div>
   );
